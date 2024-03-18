64902b5c457bc0ff05ebb43d09d57c5a87223bf1
Hanan <hanan.ashrafi@lycee-faure.fr>
2024-03-18 17:29:17 +0100

    mettre en place la page d'authentification

--- app/src/main/java/com/example/gsb_visites/GsbVisitesServices.java
+++ app/src/main/java/com/example/gsb_visites/GsbVisitesServices.java
@@ -2,8 +2,12 @@ package com.example.gsb_visites;
 
 import retrofit2.Call;
 
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface GsbVisitesServices {
+    @POST("api/auth/login")
+    Call<Visiteur> login(@Body Visiteur visiteur);
 }
--- app/src/main/java/com/example/gsb_visites/MainActivity.java
+++ app/src/main/java/com/example/gsb_visites/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gsb_visites.databinding.ActivityMainBinding;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
+    private Visiteur visiteur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        binding.btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                visiteur = new Visiteur();
+                visiteur.setEmail(binding.editTextEmail.getText().toString());
+                visiteur.setPassword(binding.editTextPassword.getText().toString());
+
+                GsbVisitesServices gsbVisitesServices = RetroFitClientInstance.getRetrofitInstance().create(GsbVisitesServices.class);
+                Call<Visiteur> call = gsbVisitesServices.login(visiteur);
+
+                call.enqueue(new Callback<Visiteur>() {
+                    @Override
+                    public void onResponse(Call<Visiteur> call, Response<Visiteur> response) {
+                        visiteur = response.body();
+                        Toast.makeText(MainActivity.this, "Connexion réussie !", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onFailure(Call<Visiteur> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Une erreur est survenue !", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
     }
 }
